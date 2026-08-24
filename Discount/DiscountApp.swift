a047ec4bf2624f12d520b7b85a093a1eb1d7f2fa
Aekkawin Anantrakarnkit <aekkawin.anantrakarnkit@gmail.com>
2026-08-24 13:46:56 +0700

    Initial commit

---
+++ Discount/DiscountApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscountApp.swift
+//  Discount
+//
+//  Created by NOTE on 8/24/26.
+//
+
+import SwiftUI
+
+@main
+struct DiscountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
